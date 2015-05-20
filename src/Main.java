bc17f277e19780eb302916862c5a458a4a9d4694
Nils Nordin <nils.nordin@me.com>
2015-05-20 20:31:57 +0200

    Hej

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@
  * Created by markus on 20/05/15.
  */
 public class Main {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        System.out.println("Good fish smell like old pussy");
+    }
 }
