0bfaa59f024f19ee99c517c9cd7ac039e5d803ab
Markus <marol825@student.liu.se>
2015-05-21 11:54:41 +0200

    Working example of Postagging
    Changed the Main class to show a working example of Postagging

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
+import java.io.IOException;
+
 /**
  * Created by markus on 20/05/15.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException,ClassNotFoundException {
+        // Initialize the tagger
 
-        System.out.println("Good fish smell like old pussy");
+        MaxentTagger tagger = new MaxentTagger("taggers/english-bidirectional-distsim.tagger");
+
+        // The sample string
+
+        String sample = "This is a sample text";
+
+        // The tagged string
+
+        String tagged = tagger.tagString(sample);
+
+        // Output the result
+
+        System.out.println(tagged);
 
 
     }
